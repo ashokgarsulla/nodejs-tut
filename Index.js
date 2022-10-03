fa0e31881da52936e3852c5e3662c25772b6a696
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-03 15:10:21 +0530

    Basic server is created

--- Index.js
+++ Index.js
@@ -1,30 +1,8 @@
-const http = require('http');
-const fs = require('fs')
-
-// const hostname = '127.0.0.1';
-const port = process.env.PORT;
+const express = require("express");
+const app = express();
+const PORT = 4000;
 
 
-const home = fs.readFileSync("files/Index.html","utf-8");
-const about = fs.readFileSync("files/about.html","utf-8");
-const contact = fs.readFileSync("files/contact.html","utf-8");
-const server = http.createServer((req, res) => {
-    if(req.url==="/"){
-         return res.end(home);
-    }
-    if(req.url==="/about"){
-        return res.end(about);
-    }
-    if(req.url==="/contact"){
-        return res.end(contact);
-    }
-    else {
-
-        return res.end("<h1>404 not Found </h1>")
-    }
-    
-});
-
-server.listen(port, () => {
-  console.log(`Server running `);
-});
+app.listen(PORT,()=> {
+    console.log("Server is working ");
+})
