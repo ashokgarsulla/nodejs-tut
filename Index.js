ca7f3a05961e77459e2770b99b3781cde73df881
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-03 15:24:59 +0530

    get request

--- Index.js
+++ Index.js
@@ -3,6 +3,27 @@ const app = express();
 const PORT = 4000;
 
 
+
+app.get("/",(req,res)=>{
+
+    res.send("<h1>This is Get request home</h1>")
+})
+
+app.get("/about",(req,res)=>{
+
+    res.send("<h1>This is about</h1>")
+})
+
+app.get("/contact",(req,res)=>{
+
+    res.send("<h1>This is Get contact</h1>")
+})
+
+app.get("/service",(req,res)=>{
+
+    res.send("<h1>This is service</h1>")
+})
+
 app.listen(PORT,()=> {
     console.log("Server is working ");
 })
