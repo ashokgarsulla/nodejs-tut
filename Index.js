3fefb8438b7e4e33d3bc23cab2a87716cfaf6a7e
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-03 20:10:17 +0530

    router is used

--- Index.js
+++ Index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const router = require("./UserRoutes");
 
 
 
@@ -9,7 +10,7 @@ const PORT = 4000;
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.json())
-
+app.use(router)
 
 app.get("/",(req,res)=>{
 
